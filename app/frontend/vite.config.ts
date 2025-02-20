0d07a141366e57a36c0b7cd88a9199e67c83889b
unknown <endlessloop1>
2025-02-20 16:49:09 -0700

    test1

--- app/frontend/vite.config.ts
+++ app/frontend/vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: true,
+    port: 3000,
+    strictPort: true,
     proxy: {
       '/api': {
         target: 'http://backend:8000',
