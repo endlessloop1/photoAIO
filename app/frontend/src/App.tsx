6a2b127e406563384f0ae98263c6f744a2a5ca60
unknown <endlessloop1>
2025-02-20 17:10:37 -0700

    test1

--- app/frontend/src/App.tsx
+++ app/frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import SystemStatus from './components/SystemStatus'
-import { QueryClient, QueryClientProvider } from 'react-query'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 const queryClient = new QueryClient()
 
